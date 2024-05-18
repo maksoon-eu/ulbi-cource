13c159e8ed3607dcb1ba9d825ffb92e68fba4431
maksoon-eu <baranovmaks05@gmail.com>
2024-05-18 12:54:04 +0300

    fix errors

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -12,9 +12,7 @@ export const Navbar = ({ className }: NavbarProps) => {
 
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                
-            </div>
+            <div className={cls.links} />
         </div>
     );
 };
