b474d670f6c2d47f87611d73f879a9a8fb504a75
Maksim <baranovmaks05@gmail.com>
2024-06-21 21:07:09 +0300

    fix screenshot test

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -15,8 +15,6 @@ const MainPage = () => {
     return (
         <div>
             {t('Главная страница')}
-            <Input placeholder="Введите текст" value={value} onChange={onChange} />
-            <Counter />
         </div>
     );
 };
